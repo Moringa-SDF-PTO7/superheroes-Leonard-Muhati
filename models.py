b5dc3835ba5535b85a440a5262ebf63e5284b963
lionard <leonard.muhati@student.moringaschool.com>
2024-11-27 16:16:15 +0300

    compelete

--- models.py
+++ models.py
@@ -1,29 +1,4 @@
-# # models.py
-# from app import db
-# from sqlalchemy.orm import validates
 
-# class Hero(db.Model):
-#     __tablename__ = 'heroes'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     super_name = db.Column(db.String(100), nullable=False)
-
-# class Power(db.Model):
-#     __tablename__ = 'powers'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     description = db.Column(db.String(255), nullable=False)
-    
-# class HeroPower(db.Model):
-#     __tablename__ = 'hero_powers'
-#     id = db.Column(db.Integer, primary_key=True)
-#     strength = db.Column(db.String(50))
-#     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
-#     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
-
-#     # Relationships
-#     hero = db.relationship('Hero', backref=db.backref('hero_powers', lazy=True))
-#     power = db.relationship('Power', backref=db.backref('hero_powers', lazy=True))
 
 
 from database import db 
